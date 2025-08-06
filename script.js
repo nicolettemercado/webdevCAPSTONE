31f16b576d14aa98d75ba3ea3d744d495cee8698
Nicolette Mercado <nicolette.em@icloud.com>
2025-08-05 22:30:42 -0400

    made dog profile cards interactive

--- script.js
+++ script.js
@@ -19,25 +19,29 @@ const dogProfiles = [
     name: "King",
     breed: "Belgian Malinois",
     age: 11,
-    bio: "Fiercely loyal and sweet. He loves his pack more than anything... well aside from chicken."
+    bio: "Fiercely loyal and sweet. He loves his pack more than anything... well aside from chicken.",
+    photo: "images/king.png"
   },
   {
     name: "Mizuki",
     breed: "Rotsky",
     age: 1,
-    bio: "Independent but sweet and well-trained with a wild streak, she's the best girl Mizu!"
+    bio: "Independent but sweet and well-trained with a wild streak, she's the best girl Mizu!",
+    photo: "images/mizuki.jpg"
   },
   {
     name: "Denali",
     breed: "Flat-Coated Retriever",
     age: 1,
-    bio: "A very handsome boy. Loves sticks. Great with kids. Avid fan of morning walks"
+    bio: "A very handsome boy. Loves sticks. Great with kids. Avid fan of morning walks",
+    photo: "images/denali.jpg"
   },
   {
     name: "Maple",
     breed: "Golden Retriever",
     age:1,
-    bio: "Tiny paws, big personality and tons of wiggles. She's sunshine in a puppy."
+    bio: "Tiny paws, big personality and tons of wiggles. She's sunshine in a puppy.",
+    photo: "images/maple.jpg"
   }
 ];
 
@@ -47,11 +51,21 @@ function displayProfiles() {
     const card = document.createElement('div');
     card.className = 'profile-card';
     card.innerHTML = `
-      <h3>${dog.name} (${dog.age} yrs)</h3>
-      <p><strong>Breed:</strong> ${dog.breed}</p>
-      <p>${dog.bio}</p>
+      <div class="card-inner">
+        <div class="card-front">
+          <img src="${dog.photo}" alt="${dog.name}" class="dog-photo" />
+        </div>
+        <div class="card-back">
+          <h3>${dog.name} (${dog.age} yrs)</h3>
+          <p><strong>Breed:</strong> ${dog.breed}</p>
+          <p>${dog.bio}</p>
+        </div>
+      </div>
     `;
     container.appendChild(card);
   });
 }
+
+
+
 
